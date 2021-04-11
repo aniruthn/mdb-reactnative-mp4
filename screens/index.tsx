9c0e286323f7164b25fa988bea4a37c976a5bada
Surya Rajan <suryarajan@berkeley.edu>
2021-04-11 16:24:36 -0700

    push notifs chunks

--- screens/index.tsx
+++ screens/index.tsx
@@ -78,3 +78,24 @@ function RootNavigator() {
   );
 }
 
+let expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
+
+// Create the messages that you want to send to clients
+let messages = [];
+for (let pushToken of []) {
+  // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+  // Check that all your push tokens appear to be valid Expo push tokens
+  if (!Expo.isExpoPushToken(pushToken)) {
+    console.error(`Push token ${pushToken} is not a valid Expo push token`);
+    continue;
+  }
+
+  // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+  messages.push({
+    to: pushToken,
+    sound: 'default',
+    body: 'This is a test notification',
+    data: { withSome: 'data' },
+  })
+}
