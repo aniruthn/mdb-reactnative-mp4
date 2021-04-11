b49d650bb4c7ec3467099e20077d369c9b918148
Surya Rajan <suryarajan@berkeley.edu>
2021-04-11 01:41:26 -0700

    notif test, some wack errors need to be fixed

--- screens/RootStack/MainScreen/MainScreen.tsx
+++ screens/RootStack/MainScreen/MainScreen.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import {Button} from "react-native";
 import Notifications from "expo-notifications";
 import Constants from "expo-constants";
 import { MainStyles } from "./MainScreenStyles";
 import { Text, View } from "../../../components/Themed";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function MainScreen() {
-  Notifications.addNotificationResponseReceivedListener((notification) => {
-    console.log(notification);
-    console.log("push verify");
-    console.log("Notification recieved");
-    const chatId = notification.notification.request.content.data.name as String;
-    console.log(chatId);
-  });
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token as string));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => setNotification(notification as boolean));
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
   return (
     <View style={MainStyles.container}>
       <Text style={MainStyles.title}>Main Screen</Text>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification();
+        }}
+      />
       <View
         style={MainStyles.separator}
         lightColor="#eee"
@@ -22,4 +49,46 @@ export default function MainScreen() {
       />
     </View>
   );
+}
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
 }
--- screens/index.tsx
+++ screens/index.tsx
@@ -77,3 +77,4 @@ function RootNavigator() {
     </Stack.Navigator>
   );
 }
+
