1f87ecfbde62c678f0740515db4d8cbcb05fa24e
Aniruth Narayanan <aniruth.n@berkeley.edu>
2021-04-17 14:13:07 -0400

    still needs to be fixed - temporarily uses start location but probably isn't needed if implemented in the main screen to get current location

--- frontend/api/auth.ts
+++ frontend/api/auth.ts
@@ -3,23 +3,28 @@ import * as geofirestore from "geofirestore";
 const firebase = require("firebase/app");
 require("firebase/firestore");
 
+//used to be locations instead of start locations
+//start locations used because it should only be tracking where a user is starting
+
 export async function setUserLocation(
     coordinates: firebase.firestore.GeoPoint
   ) {
     const firestore = firebase.firestore();
     const GeoFirestore = geofirestore.initializeApp(firestore);
-    const geocollection = GeoFirestore.collection("locations");
-    const userId = firebase.auth().currentUser.uid;
-    geocollection.doc(userId).set({
-      coordinates: coordinates,
-      lastUpdated: Date.now(),
-      userId: userId,
-      requested: false,
-    });
+    const geocollection = GeoFirestore.collection("startLocations");
+    const userId = firebase.auth().currentUser?.uid;
+    geocollection.doc(userId).set(
+      {
+        coordinates: coordinates,
+        lastUpdated: Date.now(),
+        requested: false,
+      }, 
+      { merge: true }
+    );
   }
   
   export async function clearUserLocation() {
     const firestore = firebase.firestore();
-    const userId = firebase.auth().currentUser.uid;
-    firestore.collection("locations").doc(userId).delete();
+    const userId = firebase.auth().currentUser?.uid;
+    firestore.collection("startLocations").doc(userId).delete();
   }
