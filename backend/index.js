b3835c09a6e26c134394ef4b0033dc8c983588c0
Aniruth Narayanan <aniruth.n@berkeley.edu>
2021-04-17 14:11:20 -0400

    backend works

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 //to run: use node index.js
-console.log('made it');
+console.log("made it");
 
 /**
  * Module dependencies.
@@ -17,30 +17,30 @@ const express = require("express");
 //  const expressStatusMonitor = require('express-status-monitor');
 //  const sass = require('node-sass-middleware');
 //  const cors = require('cors');
- 
- /**
-  * Load environment variables from .env file, where API keys and passwords are configured.
-  */
+
+/**
+ * Load environment variables from .env file, where API keys and passwords are configured.
+ */
 //  dotenv.load({ path: '.env' });
- 
- /**
-  * Controllers (route handlers).
-  */
+
+/**
+ * Controllers (route handlers).
+ */
 //  import { postNotification } from './server.js';
- const serv = require('./server.js');
- const postNotification = serv.postNotification;
- /**
-  * Create Express server.
-  */
- const app = express();
- const router = express.Router();
- 
- /**
-  * Express configuration.
-  */
- app.set('host', '0.0.0.0');
- app.set('port', 8080);
- app.use(express.json());
+const serv = require("./server.js");
+const postNotification = serv.postNotification;
+/**
+ * Create Express server.
+ */
+const app = express();
+const router = express.Router();
+
+/**
+ * Express configuration.
+ */
+app.set("host", "0.0.0.0");
+app.set("port", 8080);
+app.use(express.json());
 //  app.set('views', path.join(__dirname, 'views'));
 //  app.set('view engine', 'pug');
 //  app.use(cors());
@@ -54,41 +54,42 @@ const express = require("express");
 //  app.use(bodyParser.json());
 //  app.use(bodyParser.urlencoded({ extended: true }));
 //  app.use(expressValidator());
- 
+
 //  app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
- 
- 
- /**
-  * API examples routes.
-  */
-app.get('/', (req, resp) => {
-  console.log('contacted the server.');
-  resp.send({'some': 'json'});
+
+/**
+ * API examples routes.
+ */
+app.get("/", (req, resp) => {
+  console.log("contacted the server.");
+  resp.send({ some: "json" });
 });
 
 //should be changed to post lmao
 // each query parameter is separated with a /:
- app.get('/sendPush/:uid/:targetlocation', (req, resp) => {
-   console.log('initial reach');
-   postNotification(req, resp)
-  });
- 
- /**
-  * Error Handler.
-  */
+app.get(
+  "/sendPush/:uid/:startLocationLat/:startLocationLong/:targetLocationLat/:targetLocationLong",
+  (req, resp) => {
+    postNotification(req, resp);
+  }
+);
+
+/**
+ * Error Handler.
+ */
 //  app.use(errorHandler());
- 
- /**
-  * Start Express server.
-  */
- app.listen(app.get('port'), () => {
-  var os = require('os');
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get("port"), () => {
+  var os = require("os");
   var networkInterfaces = os.networkInterfaces;
   console.log(networkInterfaces());
-   console.log('App is running at http://localhost:%s', app.get('port'));
-   console.log('  Press CTRL-C to stop\n');
- });
- 
+  console.log("App is running at http://localhost:%s", app.get("port"));
+  console.log("  Press CTRL-C to stop\n");
+});
+
 module.exports = router;
 
 //  export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 // const User = require('../models/User');
 // import { Expo } from 'expo-server-sdk';
 // import firebase from "firebase";
-const expoServer = require('expo-server-sdk');
+const expoServer = require("expo-server-sdk");
 const Expo = expoServer.Expo;
-const firebase = require('firebase');
+const firebase = require("firebase");
 
-const geofirestore = require('geofirestore');
+const geofirestore = require("geofirestore");
 
 const firebaseConfig = require("../keys.json");
 
@@ -28,7 +28,9 @@ if (firebase.apps.length == 0) {
 const db = firebase.firestore();
 
 const GeoFirestore = geofirestore.initializeApp(db);
-const geocollection = GeoFirestore.collection('locations');
+// const geocollection = GeoFirestore.collection("locations");
+const GeoCollectionStartLocations = GeoFirestore.collection("startLocations");
+const GeoCollectionTargetLocations = GeoFirestore.collection("targetLocations");
 
 // geocollection.add({
 //   name: 'Geofirestore',
@@ -37,10 +39,9 @@ const geocollection = GeoFirestore.collection('locations');
 //   coordinates: new firebase.firestore.GeoPoint(40.7589, -73.9851)
 // })
 
-
 // Create a new Expo SDK client
 const expo = new Expo();
-console.log('made it');
+console.log("made it");
 
 /**
  * POST /user
@@ -77,100 +78,158 @@ console.log('made it');
  * Send a push notification to a user
  */
 exports.postNotification = (req, response) => {
-  const uid = req.params.uid;
-  console.log(uid);
-  const targetLocation = req.params.targetLocation;
-  console.log(targetLocation);
+  const {
+    uid,
+    startLocationLat,
+    startLocationLong,
+    targetLocationLat,
+    targetLocationLong,
+  } = req.params;
 
-  // PLEASE READ: This is what I think is going wrong below
-  // The line where it's sending push notifications async is erroring, maybe because of type or something else
-  // I think we need to stick closer to this example https://github.com/expo/expo-server-sdk-node because everything
-  // else (firebase stuff) I managed to get work along with a few of the other errors
-  console.log('reached');
-  //query the database to find the user who is closest to the user with the current uid
-  // this uid passed into doc represents the person receiving the notification
-  // Create a GeoQuery based on a location
-  db.collection("locations").doc(uid).get().then((doc) => {
-    const data = doc.data();
-    const query = geocollection.near({ center: data.coordinates, radius: 100 });
-    //query will always be defined since it'll query the own user too
+  const startLocation = new firebase.firestore.GeoPoint(
+    Number(startLocationLat),
+    Number(startLocationLong)
+  );
+  const targetLocation = new firebase.firestore.GeoPoint(
+    Number(targetLocationLat),
+    Number(targetLocationLong)
+  );
 
-    // Get query (as Promise)
-    query.get().then((value) => {
-      // All GeoDocument returned by GeoQuery, like the GeoDocument added above
+  db.collection("users")
+    .doc(uid)
+    .get()
+    .then((currentUserInfo) => {
+      const startQuery = GeoCollectionStartLocations.near({
+        center: startLocation,
+        radius: 100,
+      }).where("requested", "==", true);
+      const targetQuery = GeoCollectionTargetLocations.near({
+        center: targetLocation,
+        radius: 100,
+      }).where("requested", "==", true);
+      //query will always be defined since it'll query the own user too
 
-      //logic will need to be implemented here to make sure it's not the same user
-      const ref = value.docs[0]; //arbitrarily pick the first user
+      // Get query (as Promise)
+      startQuery
+        .get()
+        .then((startValue) => {
+          // All GeoDocument returned by GeoQuery, like the GeoDocument added above
+          targetQuery.get().then((targetValue) => {
+            //make sure the other user is not the same user and is arbitrarily close with a close enough
+            //target location
 
-      // if the only value for ref is just the initial user that means the request is brand new
-      // and needs to be built now
+            const startUIDs = startValue.docs;
+            let targetUIDs = [];
+            targetValue.docs.forEach((val) => {
+              targetUIDs.push(val.id);
+            });
+            const findOtherUID = () => {
+              for (let i = 0; i < startUIDs.length; i++) {
+                if (
+                  uid != startUIDs[i].id &&
+                  startUIDs &&
+                  targetUIDs.includes(startUIDs[i].id)
+                ) {
+                  return startUIDs[i];
+                }
+              }
+              return undefined;
+            };
 
+            const ref = findOtherUID();
 
-      console.log(ref); //references the uid of which user is token for confirmation
-      // ref.data.then((doc) => {
-      let otherId = ref.id;
-      console.log(otherId);
-      const docRef = db.collection("users").doc(otherId);
-      docRef.get().then((doc) => {
-        console.log(doc.data());
-        const token = doc.data().notificationToken;
-        const otherName = doc.data().name;
-        //token is right!!!!
-        console.log(token);
-    
-      let somePushTokens = [token];
-    
-      let messages = [];
-      for (let pushToken of somePushTokens) {
-      // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
-    
-      // Check that all your push tokens appear to be valid Expo push tokens
-        if (!Expo.isExpoPushToken(pushToken)) {
-          console.error(`Push token ${pushToken} is not a valid Expo push token`);
-          continue;
-        }
+            if (ref == undefined) {
+              //don't send notifications, there isn't another user on the same path unfortunately
+              console.log("No other user wants that route.");
+              response.send({ msg: "No other user wants that route." });
+              return;
+            }
 
-      // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
-        messages.push({
-          to: pushToken,
-          sound: 'default',
-          body: `${otherName} is your match!`,
-          data: { withSome: 'data' },
+            db.collection("users")
+              .doc(ref.id)
+              .get()
+              .then((doc) => {
+                //grabs tokens and names
+                const otherToken = doc.data().notificationToken;
+                const otherName = doc.data().name;
+                const currentToken = currentUserInfo.data().notificationToken;
+                const currentName = currentUserInfo.data().name;
+
+                let somePushTokens = [currentToken, otherToken];
+                let names = [otherName, currentName];
+
+                let messages = [];
+                for (let i = 0; i < somePushTokens.length; i++) {
+                  // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+                  const pushToken = somePushTokens[i];
+                  const name = names[i];
+
+                  // Check that all your push tokens appear to be valid Expo push tokens
+                  if (!Expo.isExpoPushToken(pushToken)) {
+                    console.error(
+                      `Push token ${pushToken} is not a valid Expo push token`
+                    );
+                    continue;
+                  }
+
+                  // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+                  messages.push({
+                    to: pushToken,
+                    sound: "default",
+                    body: `${name} is your match!`,
+                    data: { withSome: "data" },
+                  });
+                }
+
+                // The Expo push notification service accepts batches of notifications so
+                // that you don't need to send 1000 requests to send 1000 notifications. We
+                // recommend you batch your notifications to reduce the number of requests
+                // and to compress them (notifications with similar content will get
+                // compressed).
+                let chunks = expo.chunkPushNotifications(messages);
+                let tickets = [];
+                (async () => {
+                  // Send the chunks to the Expo push notification service. There are
+                  // different strategies you could use. A simple one is to send one chunk at a
+                  // time, which nicely spreads the load out over time:
+                  for (let chunk of chunks) {
+                    try {
+                      let ticketChunk = await expo.sendPushNotificationsAsync(
+                        chunk
+                      );
+                      console.log(ticketChunk);
+                      tickets.push(...ticketChunk);
+                      // NOTE: If a ticket contains an error code in ticket.details.error, you
+                      // must handle it appropriately. The error codes are listed in the Expo
+                      // documentation:
+                      // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+                    } catch (error) {
+                      console.error(error);
+                    }
+                  }
+                })();
+              });
+            db.collection("startLocations").doc(uid).update({
+              requested: false,
+            });
+            db.collection("targetLocations").doc(uid).update({
+              requested: false,
+            });
+            db.collection("startLocations").doc(ref.id).update({
+              requested: false,
+            });
+            db.collection("targetLocations").doc(ref.id).update({
+              requested: false,
+            });
+            response.send({ msg: "Worked!", otherUID: ref.id });
+          });
         })
-      }
-    
-      // The Expo push notification service accepts batches of notifications so
-      // that you don't need to send 1000 requests to send 1000 notifications. We
-      // recommend you batch your notifications to reduce the number of requests
-      // and to compress them (notifications with similar content will get
-      // compressed).
-      let chunks = expo.chunkPushNotifications(messages);
-      let tickets = [];
-      (async () => {
-        // Send the chunks to the Expo push notification service. There are
-        // different strategies you could use. A simple one is to send one chunk at a
-        // time, which nicely spreads the load out over time:
-        for (let chunk of chunks) {
-          try {
-            let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-            console.log(ticketChunk);
-            tickets.push(...ticketChunk);
-            // NOTE: If a ticket contains an error code in ticket.details.error, you
-            // must handle it appropriately. The error codes are listed in the Expo
-            // documentation:
-            // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
-          } catch (error) {
-            console.error(error);
-          }
-        }
-      })();
+        .catch((error) => {
+          console.error(error);
+          response.send({ msg: "Didn't work!" });
+        });
     });
-    // });
-  }).catch((error) => {
-    console.error(error);
-  });
-
-})};
+};
 
 // exports.postNotification = (req: { params: { uid: any; location1: any; location2: any;}; body: { title: any; message: any; }; }, res: { json: (arg0: any) => void }) => {
 //   const uid: string = req.params.uid;
@@ -196,7 +255,7 @@ exports.postNotification = (req, response) => {
 //     }
 //   }).catch((error: any) => {
 //     console.error(error);
-//   }); 
+//   });
 // };
 
 /*
